13fdbfaa07d7dabecfed7a32b9de9363de5e881e
Christian Agodomou <christian.agodomou@etu.hesge.ch>
2025-12-03 16:03:18 +0100

    Json for db

--- src/testAPI.ts
+++ src/testAPI.ts
@@ -1,5 +1,43 @@
 import HLTV from 'hltv'
-console.log("hello");
-HLTV.getPlayer({ id: 7322 }).then(res => {
-    console.log(res);
+//console.log("hello");
+HLTV.getPlayer({ id: 19187, }).then(res => {
+    const json = JSON.stringify(res, null);
+
+    fs.writeFile(`player${res.ign}.json`, json, 'utf8', (err: any)=> {
+        if (err) {
+            console.error('Error writing file', err);
+        } else {
+            console.log('File successfully saved!');
+        }
+    }) 
+})
+
+HLTV.getTeam({ id: 7175 }).then(res => {
+    //console.log(res)
+
+    const json = JSON.stringify(res, null);
+
+    fs.writeFile(`team${res.name}.json`, json, 'utf8', (err: any)=> {
+        if (err) {
+            console.error('Error writing file', err);
+        } else {
+            console.log('File successfully saved!');
+        }
+    }) 
+})
+
+const fs = require('fs');
+
+HLTV.getTeamRanking({ year: 2022, month: 'december', day: 26}).then(res => {
+    //console.log(res)
+
+    const json = JSON.stringify(res, null);
+
+    fs.writeFile(`rank.json`, json, 'utf8', (err: any)=> {
+        if (err) {
+            console.error('Error writing file', err);
+        } else {
+            console.log('File successfully saved!');
+        }
+    }) 
 })
