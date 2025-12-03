04e5dd5676526a9be1c508b2f6210397bdc19796
Christian Agodomou <christian.agodomou@etu.hesge.ch>
2025-12-03 16:38:08 +0100

    Json cleaned

--- src/testAPI.ts
+++ src/testAPI.ts
@@ -1,6 +1,7 @@
 import HLTV from 'hltv'
 //console.log("hello");
-HLTV.getPlayer({ id: 19187, }).then(res => {
+const fs = require('fs');
+HLTV.getPlayer({ id: 16349, }).then(res => {
     const json = JSON.stringify(res, null);
 
     fs.writeFile(`player${res.ign}.json`, json, 'utf8', (err: any)=> {
@@ -12,32 +13,32 @@ HLTV.getPlayer({ id: 19187, }).then(res => {
     }) 
 })
 
-HLTV.getTeam({ id: 7175 }).then(res => {
-    //console.log(res)
+// HLTV.getTeam({ id: 7175 }).then(res => {
+//     console.log(res)
 
-    const json = JSON.stringify(res, null);
+//     const json = JSON.stringify(res, null);
 
-    fs.writeFile(`team${res.name}.json`, json, 'utf8', (err: any)=> {
-        if (err) {
-            console.error('Error writing file', err);
-        } else {
-            console.log('File successfully saved!');
-        }
-    }) 
-})
+//     fs.writeFile(`team${res.name}.json`, json, 'utf8', (err: any)=> {
+//         if (err) {
+//             console.error('Error writing file', err);
+//         } else {
+//             console.log('File successfully saved!');
+//         }
+//     }) 
+// })
 
-const fs = require('fs');
 
-HLTV.getTeamRanking({ year: 2022, month: 'december', day: 26}).then(res => {
-    //console.log(res)
 
-    const json = JSON.stringify(res, null);
+// HLTV.getTeamRanking({ year: 2022, month: 'december', day: 26}).then(res => {
+//     console.log(res)
 
-    fs.writeFile(`rank.json`, json, 'utf8', (err: any)=> {
-        if (err) {
-            console.error('Error writing file', err);
-        } else {
-            console.log('File successfully saved!');
-        }
-    }) 
-})
+//     const json = JSON.stringify(res, null);
+
+//     fs.writeFile(`rank.json`, json, 'utf8', (err: any)=> {
+//         if (err) {
+//             console.error('Error writing file', err);
+//         } else {
+//             console.log('File successfully saved!');
+//         }
+//     }) 
+// })
