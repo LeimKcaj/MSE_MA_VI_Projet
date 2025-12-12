5714d52b941f7063bdc98baf0974c17ef605ef00
benjamin.anthonio <benjamin.anthonioz@etu.hesge.ch>
2025-12-12 16:48:36 +0100

    add players

--- src/testAPI.ts
+++ src/testAPI.ts
@@ -1,35 +1,131 @@
 import HLTV from 'hltv'
 //console.log("hello");
 const fs = require('fs');
-// HLTV.getPlayer({ id: 16349, }).then(res => {
-//     const json = JSON.stringify(res, null);
 
-//     fs.writeFile(`player${res.ign}.json`, json, 'utf8', (err: any)=> {
-//         if (err) {
-//             console.error('Error writing file', err);
-//         } else {
-//             console.log('File successfully saved!');
-//         }
-//     }) 
-// })
+let playersIDs = [
+    8183,
+16555,
+17011,
+15662,
+18510,
+7592,
+20304,
+17956,
+10096,
+9032,
+10994,
+6124,
+9618,
+18790,
+7998,
+20926,
+8918,
+21139,
+8248,
+2476,
+21815,
+12731,
+21972,
+21233,
+12708,
+7264,
+18053,
+429,
+9960,
+22383,
+10394,
+165,
+3741,
+19677,
+12018,
+19230,
+24177,
+17306,
+21,
+3972,
+11617,
+20447,
+21062,
+19164,
+7821,
+19187,
+22047,
+20119,
+23600,
+21983,
+19310,
+3820,
+18072,
+20312,
+18850,
+13666,
+18221,
+9295,
+18987,
+9816,
+14759,
+20127,
+22673,
+19206,
+472,
+19235,
+13776,
+22929,
+22471,
+22076,
+1865,
+7716,
+21167,
+16920,
+23685,
+19808,
+18317,
+20423,
+2154,
+7322,
+11893,
+16693,
+18462,
+11816,
+9086,
+7528,
+15165,
+17803,
+23046,
+23414,
+9363
+]
+for (let i = 0; i < playersIDs.length; i++) {
 
-let teamsIDs = [7175, 13380, 12878, 11283, 12467, 4494, 4608, 4991, 6667, 5995, 8474, 7020, 6665, 4869, 9565]
-for (let i = 0; i < teamsIDs.length; i++) {
-    HLTV.getTeam({ id: teamsIDs[i] as number}).then(res => {
-        console.log(res)
-    
+    HLTV.getPlayer({ id: playersIDs[i] as number }).then(res => {
         const json = JSON.stringify(res, null);
-    
-        fs.writeFile(`team${res.name}.json`, json, 'utf8', (err: any)=> {
+
+        fs.writeFile(`player${res.ign}.json`, json, 'utf8', (err: any)=> {
             if (err) {
                 console.error('Error writing file', err);
             } else {
                 console.log('File successfully saved!');
             }
-        }) 
+        })
     })
-
 }
+
+// let teamsIDs = [7175, 13380, 12878, 11283, 12467, 4494, 4608, 4991, 6667, 5995, 8474, 7020, 6665, 4869, 9565]
+// for (let i = 0; i < teamsIDs.length; i++) {
+//     HLTV.getTeam({ id: teamsIDs[i] as number}).then(res => {
+//         console.log(res)
+    
+//         const json = JSON.stringify(res, null);
+    
+//         fs.writeFile(`team${res.name}.json`, json, 'utf8', (err: any)=> {
+//             if (err) {
+//                 console.error('Error writing file', err);
+//             } else {
+//                 console.log('File successfully saved!');
+//             }
+//         }) 
+//     })
+// }
 
 
 
